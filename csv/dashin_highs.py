9dec8aadecdca827f37db25fbc5a8b7ddaba0d34
anonymousdomain <domainanonymous1@gmail.com>
2021-12-03 23:43:23 +0300

    intial commit for csv

---
+++ csv/dashin_highs.py
@@ -0,0 +1,8 @@
+import csv 
+ 
+filename='C:\\Users\public\Devo programs\Data_visualization\csv\data\dashin_weather_07-2018_simple.csv'
+
+with open(filename) as f:
+    reader=csv.reader(f)
+    header_row=next(reader)
+    print(header_row)
