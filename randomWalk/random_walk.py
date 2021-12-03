890660bd07d4bb1f51b03648271ae68e8ad0faf9
anonymousdomain <domainanonymous1@gmail.com>
2021-12-03 20:50:18 +0300

    code rafactor

--- randomWalk/random_walk.py
+++ randomWalk/random_walk.py
@@ -7,24 +7,33 @@ class RandomWalk:
         
         self.x_values=[0]
         self.y_values=[0]
+        
     def fill_walk(self):
        while len(self.x_values)<self.num_points:
+           
+            x_step=self.get_xstep()
+          
+            y_step=self.get_ystep()
+            if x_step==0 and y_step==0:
+                continue
+            
+            #calculate the new postion
+            self.new_postion()
+            
+    def get_xstep(self):
+        
         x_direction=choice([1,-1])
         x_distance=choice([0,1,2,3,4,5])
-        
-        x_step=x_direction*x_distance
-        
+    
+        return x_direction*x_distance
+    def get_ystep(self):
         y_direction=choice([1,-1])
-        y_distance=choice([0,1,2,3,4,5])
+        y_distance=choice([1,2,3,4,5])
         
-        y_step=y_direction*y_distance
-        
-        if x_step==0 and y_step==0:
-            continue
-        
-        #calculate the new postion
-        x=self.x_values[-1]+x_step
-        y=self.y_values[-1]+y_step
-        
+        return y_direction*y_distance
+    def new_postion(self):
+        x=self.x_values[-1]+self.get_xstep()
+        y=self.y_values[-1]+self.get_ystep()
+            
         self.x_values.append(x)
         self.y_values.append(y)
