a6a57a9d52e00068a4b263c1c11039bafc405098
anonymousdomain <domainanonymous1@gmail.com>
2021-12-03 20:02:37 +0300

    style starting point and ending points

--- randomWalk/random_visual.py
+++ randomWalk/random_visual.py
@@ -7,7 +7,11 @@ rw.fill_walk()
 plt.style.use('classic')
 fig ,ax=plt.subplots()
 point_nums=range(rw.num_points)
+ax.scatter(0,0,c='red',edgecolors='none',s=100)
+ax.scatter(rw.x_values[-1],rw.y_values[-1],c='blue',edgecolors='none',s=100)
 ax.scatter(rw.x_values,rw.y_values,c=point_nums,cmap=plt.cm.Blues,edgecolors='none',s=10)
-
-#plt.show();
-plt.savefig('img\\random_walk.png',bbox_inches='tight')
+#get rid of the axis
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+plt.show();
+#plt.savefig('img\\random_walk.png',bbox_inches='tight')
