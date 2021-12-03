deb20e98bff411db6a90e4ce2485c2c7a85a7205
anonymousdomain <domainanonymous1@gmail.com>
2021-12-04 00:11:00 +0300

    extract maxtemp and datetime

--- csv/dashin_highs.py
+++ csv/dashin_highs.py
@@ -1,8 +1,15 @@
-import csv 
- 
-filename='C:\\Users\public\Devo programs\Data_visualization\csv\data\dashin_weather_07-2018_simple.csv'
+import csv
+from datetime import datetime
+filename = 'C:\\Users\public\Devo programs\Data_visualization\csv\data\dashin_weather_07-2018_simple.csv'
 
 with open(filename) as f:
-    reader=csv.reader(f)
-    header_row=next(reader)
-    print(header_row)
+    reader = csv.reader(f)
+    header_row = next(reader)
+    dates,highs=[],[]
+    for row in reader:
+        high=int(row[5])
+        current_datetime=datetime.strptime(row[2],'%Y-%m-%d')
+        highs.append(high)
+        dates.append(current_datetime)
+
+
