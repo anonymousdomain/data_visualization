ef764793bde3995267ad5d910cc215b5252003b8
anonymousdomain <domainanonymous1@gmail.com>
2021-12-04 00:26:47 +0300

    extricted data is plotted

--- csv/dashin_highs.py
+++ csv/dashin_highs.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+import matplotlib.pyplot as plt
 filename = 'C:\\Users\public\Devo programs\Data_visualization\csv\data\dashin_weather_07-2018_simple.csv'
 
 with open(filename) as f:
@@ -12,4 +13,14 @@ with open(filename) as f:
         highs.append(high)
         dates.append(current_datetime)
 
+plt.style.use('seaborn')
+fig,ax=plt.subplots()
+ax.plot(dates,highs,c='red')
 
+plt.title('daily high temprature',fontsize=16)
+plt.xlabel('dates',fontsize=16)
+#avoid datetime overlaping in the label
+fig.autofmt_xdate()
+plt.ylabel('Temprature in(F)',fontsize=16)
+plt.tick_params(axis='both',which='major',labelsize=10)
+plt.show()
