35ec3007536ab6dba15cdbe9e0fc6e343165d365
anonymousdomain <domainanonymous1@gmail.com>
2021-12-03 21:31:58 +0300

    reformat

--- randomWalk/random_visual.py
+++ randomWalk/random_visual.py
@@ -1,20 +1,22 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw=RandomWalk(50_000)
+rw = RandomWalk(50_000)
 rw.fill_walk()
 
 plt.style.use('classic')
 
-fig ,ax=plt.subplots(figsize=(8,8))
-point_nums=range(rw.num_points)
-ax.scatter(0,0,c='red',edgecolors='none',s=100)
-ax.scatter(rw.x_values[-1],rw.y_values[-1],c='black',edgecolors='none',s=100)
-#ax.plot(rw.x_values,rw.y_values,linewidth=1)
-ax.scatter(rw.x_values,rw.y_values,c=point_nums,cmap=plt.cm.Blues,edgecolors='none',s=1)
-#get rid of the axis
+fig, ax = plt.subplots(figsize=(8, 8))
+point_nums = range(rw.num_points)
+ax.scatter(0, 0, c='red', edgecolors='none', s=100)
+ax.scatter(rw.x_values[-1], rw.y_values[-1],
+           c='black', edgecolors='none', s=100)
+# ax.plot(rw.x_values,rw.y_values,linewidth=1)
+ax.scatter(rw.x_values, rw.y_values, c=point_nums,
+           cmap=plt.cm.Blues, edgecolors='none', s=1)
+# get rid of the axis
 ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 
-plt.show();
-#plt.savefig('img\\random_walk.png',bbox_inches='tight')
+plt.show()
+# plt.savefig('img\\random_walk.png',bbox_inches='tight')
