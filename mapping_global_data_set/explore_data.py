4ff478419e7591d4b50f5471a30ac908c32f8202
anonymousdomain <domainanonymous1@gmail.com>
2021-12-04 10:14:33 +0300

    exctract magnitude of earth_quak

--- mapping_global_data_set/explore_data.py
+++ mapping_global_data_set/explore_data.py
@@ -5,3 +5,14 @@ filename='mapping_global_data_set\data\eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data=json.load(f)
     
+readable_file='mapping_global_data_set\data\readable_eq_data.json'
+    
+all_eq_dict=all_eq_data['features']
+
+mags=[]
+
+for eq_dict in all_eq_dict:
+    
+    mag=eq_dict['properties']['mag']
+    mags.append(mag)
+print(mags[:6])
