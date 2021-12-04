7fe3ad5b1a6eeaff65c56a9b9894459b58c8f91a
anonymousdomain <domainanonymous1@gmail.com>
2021-12-04 08:56:49 +0300

    comparsion b/n death_valley and dashin_highs

--- csv/dashin_highs.py
+++ csv/dashin_highs.py
@@ -2,29 +2,30 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 filename = 'C:\\Users\public\Devo programs\Data_visualization\csv\data\dashin_weather_2018_simple.csv'
+class exctract_data:
 
-with open(filename) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-    dates,highs,lows=[],[],[]
-    for row in reader:
-        high=int(row[5])
-        current_datetime=datetime.strptime(row[2],'%Y-%m-%d')
-        low=int(row[6])
-        highs.append(high)
-        dates.append(current_datetime)
-        lows.append(low)
+    with open(filename) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+        dates,highs,lows=[],[],[]
+        for row in reader:
+            high=int(row[5])
+            current_datetime=datetime.strptime(row[2],'%Y-%m-%d')
+            low=int(row[6])
+            highs.append(high)
+            dates.append(current_datetime)
+            lows.append(low)
 
-plt.style.use('seaborn')
-fig,ax=plt.subplots()
-ax.plot(dates,highs,c='red',alpha=0.5)
-ax.plot(dates,lows,c='blue',alpha=0.5)
-
-ax.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
-plt.title('daily high temprature',fontsize=16)
-plt.xlabel('dates',fontsize=16)
-#avoid datetime overlaping in the label
-fig.autofmt_xdate()
-plt.ylabel('Temprature in(F)',fontsize=16)
-plt.tick_params(axis='both',which='major',labelsize=10)
-plt.show()
+#plt.style.use('seaborn')
+#fig,ax=plt.subplots()
+#ax.plot(dates,highs,c='red',alpha=0.5)
+#ax.plot(dates,lows,c='blue',alpha=0.5)
+#
+#ax.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
+#plt.title('daily high temprature',fontsize=16)
+#plt.xlabel('dates',fontsize=16)
+##avoid datetime overlaping in the label
+#fig.autofmt_xdate()
+#plt.ylabel('Temprature in(F)',fontsize=16)
+#plt.tick_params(axis='both',which='major',labelsize=10)
+#plt.show()
--- csv/death_valley.py
+++ csv/death_valley.py
@@ -1,30 +1,33 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
-filename='csv\data\death_valley_2018_simple.csv'
+from dashin_highs import exctract_data
+filename = 'csv\data\death_valley_2018_simple.csv'
 
 with open(filename) as f:
-    reader=csv.reader(f)
-    head_row=next(reader)
-        
-    highs,lows,dates=[],[],[]
+    reader = csv.reader(f)
+    head_row = next(reader)
+
+    highs, lows, dates = [], [], []
     for row in reader:
-        current_date=datetime.strptime(row[2],'%Y-%m-%d')
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         try:
-                high=int(row[4])   
-                low=int(row[5])
-            
+            high = int(row[4])
+            low = int(row[5])
+
         except ValueError:
             print(f"missing data for {current_date}")
         else:
-                highs.append(high)
-                lows.append(low)
-                dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+            dates.append(current_date)
 
-fig,ax=plt.subplots()
-ax.plot(dates,highs,c='red',alpha=0.5)
-ax.plot(dates,lows,c='blue',alpha=0.5)
+death = exctract_data()
 
-ax.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(death.dates, death.highs, c='blue', alpha=0.5)
+
+ax.fill_between(dates, highs, death.highs, facecolor='blue', alpha=0.1)
 
 plt.show()
