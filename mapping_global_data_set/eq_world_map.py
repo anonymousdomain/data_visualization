3f84fbeb1a7c2ee8ed2a569b91f91e6961377bca
anonymousdomain <domainanonymous1@gmail.com>
2021-12-04 12:59:22 +0300

    final commit

--- mapping_global_data_set/eq_world_map.py
+++ mapping_global_data_set/eq_world_map.py
@@ -1,27 +1,24 @@
-from plotly.graph_objs import Scattergeo,Layout
+from plotly.graph_objs import Scattergeo, Layout
 
 from plotly import offline
 from explore_data import extracted_data as d
 
-#data=[Scattergeo(lon=d.lons,lat=d.lats)]
-data=[{
-    'type':'scattergeo',
-    'lon':d.lons,
-    'lat':d.lats,
-    'text':d.hover_titles,
-    'marker':{
-        'size':[5*mag for mag in d.mags],
+# data=[Scattergeo(lon=d.lons,lat=d.lats)]
+data = [{
+    'type': 'scattergeo',
+    'lon': d.lons,
+    'lat': d.lats,
+    'text': d.hover_titles,
+    'marker': {
+        'size': [5*mag for mag in d.mags],
         'color':d.mags,
         'colorscale':'Viridis',
         'reversescale':True,
-        'colorbar':{'title':'Magnitude'}
+        'colorbar':{'title': 'Magnitude'}
     }
 }]
-my_layout=Layout(title=d.all_eq_title)
+my_layout = Layout(title=d.all_eq_title)
 
-fig={'data':data,'layout':my_layout}
+fig = {'data': data, 'layout': my_layout}
 
-offline.plot(fig,filename='img\global_earthquak.html')
-
-
-
+offline.plot(fig, filename='img\global_earthquak.html')
--- mapping_global_data_set/explore_data.py
+++ mapping_global_data_set/explore_data.py
@@ -1,23 +1,20 @@
-import json 
+import json
 
-#filename='mapping_global_data_set\data\eq_data_1_day_m1.json'
-filename='mapping_global_data_set\data\eq_data_30_day_m1.json'
+# filename='mapping_global_data_set\data\eq_data_1_day_m1.json'
+filename = 'mapping_global_data_set\data\eq_data_30_day_m1.json'
 with open(filename) as f:
-    all_eq_data=json.load(f)
-    
+    all_eq_data = json.load(f)
+
+
 class extracted_data():
-    all_eq_dict=all_eq_data['features']
-    all_eq_title=all_eq_data['metadata']['title']
-    mags,lons,lats,hover_titles=[],[],[],[]
+    all_eq_dict = all_eq_data['features']
+    all_eq_title = all_eq_data['metadata']['title']
+    mags, lons, lats, hover_titles = [], [], [], []
     for eq_dict in all_eq_dict:
-        
+
         mags.append(eq_dict['properties']['mag'])
         lons.append(eq_dict['geometry']['coordinates'][0])
         lats.append(eq_dict['geometry']['coordinates'][1])
         hover_titles.append(eq_dict['properties']['title'])
-      
 
    # print(mags[:10],lons[:5],lats[:5])
-
- 
-    
