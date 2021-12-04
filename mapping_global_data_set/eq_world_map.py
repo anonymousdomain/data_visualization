d845cb5fa71a3325034ee754b35f77a29e29fe2e
anonymousdomain <domainanonymous1@gmail.com>
2021-12-04 12:39:05 +0300

    color scale darkblue shows the most mags earthquak and yellow indicates lowest mags

--- mapping_global_data_set/eq_world_map.py
+++ mapping_global_data_set/eq_world_map.py
@@ -3,8 +3,19 @@ from plotly.graph_objs import Scattergeo,Layout
 from plotly import offline
 from explore_data import extracted_data as d
 
-data=[Scattergeo(lon=d.lons,lat=d.lats)]
-
+#data=[Scattergeo(lon=d.lons,lat=d.lats)]
+data=[{
+    'type':'scattergeo',
+    'lon':d.lons,
+    'lat':d.lats,
+    'marker':{
+        'size':[5*mag for mag in d.mags],
+        'color':d.mags,
+        'colorscale':'Viridis',
+        'reversescale':True,
+        'colorbar':{'title':'Magnitude'}
+    }
+}]
 my_layout=Layout(title='Global Earthquaks')
 
 fig={'data':data,'layout':my_layout}
--- mapping_global_data_set/explore_data.py
+++ mapping_global_data_set/explore_data.py
@@ -1,7 +1,7 @@
 import json 
 
-filename='mapping_global_data_set\data\eq_data_1_day_m1.json'
-
+#filename='mapping_global_data_set\data\eq_data_1_day_m1.json'
+filename='mapping_global_data_set\data\eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data=json.load(f)
     
